6b6f038f4b0de658de498913935d05b41a7d24cc
MPetrauskas2001 <82758969+MPetrauskas2001@users.noreply.github.com>
2021-05-24 21:14:55 +0300

    Add files via upload

---
+++ project/login.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    //something was posted
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+
+    if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+    {
+
+        //read from database
+        $query = "select * from users where user_name = '$user_name' limit 1";
+
+        $result = mysqli_query($con, $query);
+        if($result)
+        {
+            if($result && mysqli_num_rows($result) > 0)
+            {
+                $user_data = mysqli_fetch_assoc($result);
+               if($user_data['password'] == $password)
+               {
+                   $_SESSION['user_id'] = $user_data['user_id'];
+                   header("Location: index.php");
+                   die;
+               }
+            }
+        }
+        echo "Wrong username or password!";
+    }else
+    {
+        echo "Wrong username or password!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+</head>
+<body>
+<div id="box">
+    <form method="post">
+        <div style="font-size: 20px;margin: 10px;color: white;">Login</div>
+<input id="text" type="text" name="user_name"><br><br>
+        <input id="text" type="password" name="password"><br><br>
+        <input id="button" type="submit" value="Login"><br><br>
+        <a href="signup.php">Click to Signup!</a><br><br>
+        <a href="index.php">Home page !</a><br><br>
+    </form>
+</div>
+        <div class="footer-basic">
+        <!-- Footer -->
+        <footer>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="index.php">Home</a></li>
+                <li class="list-inline-item"><a href="#team">Our Team</a></li>
+                <li class="list-inline-item"><a href="#about">About</a></li>
+                <li class="list-inline-item"><a href="#pricing">Pricing</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+
+            </ul>
+            <p class="copyright">Powered by Martynas Petrauskas</p>
+        </footer>
+</div>
+</div>
+</body>
+</html>
